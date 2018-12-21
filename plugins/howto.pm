5e6ccaf71d747975b2c251548a26ea3e5f83f44a
Paul Hoffman <paul@flo.org>
2018-12-21 15:54:18 -0500

    howto plugin: various enhancements

--- plugins/howto.pm
+++ plugins/howto.pm
@@ -27,20 +27,86 @@ sub hooks { }
 
 sub howto {
     my $self = shift;
-    usage($usagestr) if @ARGV < 2;
-    my ($task, $path) = splice @ARGV, 0, 2;
+    $self->app->orient;
+    if (@ARGV == 1) {
+        $self->list_howtos(@ARGV);
+    }
+    elsif (@ARGV == 2) {
+        $self->show_howto(@ARGV);
+    }
+    else {
+        $self->set_howto(@ARGV);
+    }
+}
+
+sub list_howtos {
+    my ($self, $path) = @_;
     my $app = $self->app;
-    $app->orient;
-    my $taskpath = "$path/howto/$task";
-    my $taskobj = eval { $app->object($taskpath, 1) };
-    if (!@ARGV) {
-        fatal("no information on how to $task $path") if !$taskobj;
-        my @props = $app->get($taskobj);
-        my ($cmd) = map { $_->[0] eq 'cmd' ? ($_->[1]) : () } @props;
-        my ($user) = map { $_->[0] eq 'user' ? ($_->[1]) : () } @props;
-        my ($cwd) = map { $_->[0] eq 'cwd' ? ($_->[1]) : () } @props;
-        my @env = map { $_->[0] eq 'env' ? ($_->[1]) : () } @props;
+    my @paths = $self->paths($path);
+    my %howto;
+    while (@paths) {
+        my $path = shift @paths;
+        my @children = $app->children($path.'/howto');
+        my @howtos;
+        foreach (@children) {
+            my $chpath = $_->{'path'};
+            (my $chname = $chpath) =~ s{.*/}{};
+            push @howtos, $chname;
+        }
+        $howto{$path} = \@howtos if @howtos;
+    }
+    @paths = sort keys %howto;
+    while (@paths) {
+        $path = shift @paths;
+        my @howtos = @{ $howto{$path} };
+        print $path, "\n";
+        print $_, "\n" for @howtos;
+        print "\n" if @paths;
+    }
+}
+
+sub paths {
+    my ($self, $path) = @_;
+    return ($path) if $path =~ m{^/};
+    my $dbh = $self->app->dbh;
+    my $sth = $dbh->prepare("SELECT path FROM objects WHERE path LIKE '%/' || ? || '/howto'");
+    $sth->execute($path);
+    my @paths;
+    while (my ($howtopath) = $sth->fetchrow_array) {
+        $howtopath =~ s{/howto$}{};
+        push @paths, $howtopath;
+    }
+    fatal("no such object: $path") if !@paths;
+    return @paths;
+}
+
+sub show_howto {
+    my ($self, $task, $path) = @_;
+    my $app = $self->app;
+    my @paths = $self->paths($path);
+    while (@paths) {
+        $path = shift @paths;
+        my $howtopath = "$path/howto";
+        my $taskpath = "$howtopath/$task";
+        my $taskobj = eval { $app->object($taskpath, 1) }
+            or next;
+            # or fatal("no information on how to $task $path");
+        my $howtoobj = $app->object($howtopath);
+        my @taskprops = $app->get($taskobj);
+        my @howtoprops = $app->get($howtoobj);
+        my ($cmd) = map { $_->[0] eq 'cmd' ? ($_->[1]) : () } @taskprops, @howtoprops;
+        my ($user) = map { $_->[0] eq 'user' ? ($_->[1]) : () } @taskprops, @howtoprops;
+        my ($cwd) = map { $_->[0] eq 'cwd' ? ($_->[1]) : () } @taskprops, @howtoprops;
+        my @tasknotes = map { $_->[0] eq 'note' ? ($_->[1]) : () } @taskprops;
+        my @howtonotes = map { $_->[0] eq 'note' ? ($_->[1]) : () } @howtoprops;
+        my @notes = @tasknotes ? @tasknotes : @howtonotes;
+        my %env = map {
+            $_->[0] eq 'env'
+                ? (split /=/, $_->[1], 2)
+                : ()
+        } @howtoprops, @taskprops;
         my $prompt = '$';
+        print $path, "\n";
         if (!defined $user) {
             print "[as the appropriate user]\n";
         }
@@ -48,21 +114,48 @@ sub howto {
             print "[as root]\n";
             $prompt = '#';
         }
+        else {
+            print "[as $user]\n";
+        }
         print "$prompt cd $cwd\n" if defined $cwd;
-        foreach (@env) {
-            my ($k, $v) = split /=/, $_, 2;
+        foreach my $k (sort keys %env) {
+            my $v = $env{$k};
             # TODO Expand %(...)
             print $prompt, 'export ', $k, '=', shell_quote($v), ' ';
         }
         # TODO Expand %(...)
         print $prompt, ' ', $cmd, "\n";
+        print '*** NOTE: ', $_, "\n" for @notes;
+        print "\n" if @paths;
     }
-    ### elsif ($taskobj) {
-    ###     $app->append($taskobj, ...);
-    ### }
-    ### else {
-    ###     $taskobj = $app->insert($path, ...);
-    ### }
+}
+
+sub set_howto {
+    my ($self, $task, $path) = splice @_, 0, 3;
+    $path =~ m{^/} or fatal("to set how-to information, you must supply a full path");
+    $path .= '/howto/' . $task;
+    my $app = $self->app;
+    my @params = @_;
+    if (eval { $app->object($path) }) {
+        $app->_transact(sub {
+            $app->set($path, @params);
+        });
+    }
+    else {
+        $app->_transact(sub {
+            $app->insert($path, @params);
+        });
+    }
+}
+
+sub find_objects {
+    my ($self, $name) = @_;
+    my $app = $self->app;
+    my $cpath = $app->bound('config')
+        or fatal("unable to resolve $name: \@config is not bound");
+    1;
+    return find_path_to_machine("$cpath/machine"),
+           find_path_to_software("$cpath/software");
 }
 
 1;
